596254dc1037dabcf98d3c4f088c779d339822c0
Юрий <lepescin@gmail.com>
2021-06-29 15:37:53 +0300

    Initial commit

---
+++ src/main/java/ru/lepescin/studentbookslibrary/StudentBooksLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.lepescin.studentbookslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentBooksLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentBooksLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lepescin/studentbookslibrary/StudentBooksLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lepescin.studentbookslibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentBooksLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
